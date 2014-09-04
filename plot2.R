55be6c9cc6483350fd56e055b86c12f2643634ad
kavisrm <kavi_ciet@yahoo.co.in>
2014-09-04 02:02:00 -0700

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -3,6 +3,7 @@ data_full <- read.csv("./Data/household_power_consumption.txt", header=T, sep=';
                       nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
 data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
 
+
 ## Subsetting the data
 data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
 rm(data_full)
